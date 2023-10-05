9a6260bf45142ff1c4b42eaf84bd3fc136dfc808
SullyaeroX <147029592+SullyaeroX@users.noreply.github.com>
2023-10-05 08:30:29 +0200

    Create fxmanifest.lua

---
+++ mymode/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sully'
+description 'Filip er meget gram'
+version '1.0.0'
+
+resource_type 'gametype' { name = 'My awesome game type!' }
+
+client_script 'mymode_client.lua'
